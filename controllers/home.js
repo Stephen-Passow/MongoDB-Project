41f597c1b4e0536163747e08cc4533a1faec7900
Stephen Passow <stephenpassow@Stephens-MacBook-Pro.local>
2019-10-19 18:40:06 -0400

    all homes function working and displaying on local host

--- controllers/home.js
+++ controllers/home.js
@@ -11,10 +11,44 @@ homeRouter.get('/home/new', (req, res) => {
 //get all home
 homeRouter.get('/home', (req, res) => {
   homeApi.getAllHomes()
-  res.render('home/allHomes', {allHomes})
+    .then((allHomes) => {
+      //console.log('allHomes', allHomes)
+      res.render('home/allHomes', { allHomes })
+    })
 })
 //get single home
-homeRouter.get('/home')
+homeRouter.get('/home/:id', (req, res) => {
+  homeApi.getSingleHome()
+    .then((singleHome) => {
+      console.log('home/singleHome', singleHome)
+      res.render('home/singleHome', { singleHome })
+    })
+})
+//create new home
+homeRouter.post('/home/new', (req, res) => {
+  homeApi.createHome()
+    .then((createdHome) => {
+      console.log('createdHome', createdHome)
+      res.redirect('/home')
+    })
+})
+//update existing home
+homeRouter.put('/home/:id', (req,res) => {
+  homeApi.updateHome()
+  .then((updatedHome) => {
+    console.log('updatedHome', updatedHome)
+    res.redirect(`home/${req.params.id}`)
+  })
+})
+//delete home
+homeRouter.delete('/home/:id', (req,res) => {
+  homeApi.deleteHome()
+  .then((deletedHome) => {
+    console.log('deletedHome', deletedHome)
+    res.redirect('/home')
+  })
+})
+
 module.exports = {
   homeRouter
 }
