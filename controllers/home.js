d3d1e6c55bbbd863f9b91ebdf98861ccc72e4412
Stephen Passow <stephenpassow@Stephens-MacBook-Pro.local>
2019-10-21 12:30:43 -0400

    homes fully functional and added wireframes and ERD images to README

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 
 const homeApi = require('../models/home.js')
@@ -8,41 +7,54 @@ const homeRouter = express.Router()
 homeRouter.get('/home/new', (req, res) => {
   res.render('home/newHomeForm')
 })
+//function to send the updated info to the editHome form
+homeRouter.get('/home/edit/:id', (req,res) => {
+  homeApi.getSingleHome(req.params.id)
+  .then((updated) => {
+    res.render('home/editHome', {updated})
+  })
+})
+
 //get all home
 homeRouter.get('/home', (req, res) => {
   homeApi.getAllHomes()
     .then((allHomes) => {
       //console.log('allHomes', allHomes)
       res.render('home/allHomes', { allHomes })
+      //res.json(allHomes)
     })
 })
+
 //get single home
 homeRouter.get('/home/:id', (req, res) => {
-  homeApi.getSingleHome()
+  homeApi.getSingleHome(req.params.id)
     .then((singleHome) => {
       console.log('home/singleHome', singleHome)
       res.render('home/singleHome', { singleHome })
     })
 })
+
 //create new home
-homeRouter.post('/home/new', (req, res) => {
-  homeApi.createHome()
+homeRouter.post('/home', (req, res) => {
+  homeApi.createHome(req.body)
     .then((createdHome) => {
       console.log('createdHome', createdHome)
       res.redirect('/home')
     })
 })
+
 //update existing home
 homeRouter.put('/home/:id', (req,res) => {
-  homeApi.updateHome()
+  homeApi.updateHome(req.params.id, req.body)
   .then((updatedHome) => {
     console.log('updatedHome', updatedHome)
-    res.redirect(`home/${req.params.id}`)
+    res.redirect(`/home/${req.params.id}`)
   })
 })
+
 //delete home
 homeRouter.delete('/home/:id', (req,res) => {
-  homeApi.deleteHome()
+  homeApi.deleteHome(req.params.id)
   .then((deletedHome) => {
     console.log('deletedHome', deletedHome)
     res.redirect('/home')
--- models/home.js
+++ models/home.js
@@ -37,5 +37,5 @@ module.exports = {
   getSingleHome,
   createHome,
   updateHome,
-  deleteHome
+  deleteHome,
 }
